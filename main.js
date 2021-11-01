682d5639efbd66c8ecb58a65bee124abdf2f27ec
semastep96 <semastep5@gmail.com>
2021-11-01 10:12:46 +0300

    refactoring after stream

--- main.js
+++ main.js
@@ -1,49 +1,58 @@
 function Calc(action, a, b) {
-  if (typeof a != 'number' || typeof b != 'number') { return 'Error' }
-  else if (action == 'sum') { return sum(a, b) }
-  else if (action == 'minus') { return minus(a, b) }
-  else if (action == 'multiply') {return multiply(a, b)}
-  else if (action == 'division') {return division(a, b)}
-  else if (action == 'remainder') {return remainder(a, b)}
-  else if (action == 'exponentiation') {return exponentiation(a, b)}
-  return 'unknown operator';
+  const isNotValid = isNotValidCalc(action, a, b)
+
+  if (isNotValid) { return isNotValid }
+
+  if (action == 'sum') { return a + b }
+
+  else if (action == 'minus') { return a - b }
+
+  else if (action == 'multiply') { return a * b }
+
+  else if (action == 'division') { return a / b }
+
+  else if (action == 'remainder') { return a % b }
+
+  else if (action == 'exponentiation') { return a ** b }
+
+  return 'unknown operator'
 }
 
-function sum(a, b) {
-  return a + b;
+function isNotValidCalc(action, a, b) {
+  // check a and b - numbers and not a NaN or Infinity
+  const isNotValidNumbers = typeof a !== 'number' || typeof b !== 'number' ||
+    a !== a || b !== b ||
+    a === Infinity || a === -Infinity ||
+    b === Infinity || b === -Infinity;
+
+  if (isNotValidNumbers) { return 'Error' }
+
+  //check divisible by 0
+  if ((action === 'division' || action === 'remainder') && b === 0) {
+    return 'cannot be divisible by 0'
+  }
+
+  return false
 }
 
-function minus(a, b) {
-  return a - b;
-}
 
-function multiply(a, b) {
-  return a * b;
-}
-
-function division(a, b) {
-  return a / b;
-}
-
-function remainder(a, b) {
-  return a % b;
-}
-
-function exponentiation(a, b) {
-  return a ** b;
-}
 
 console.log(`\n
 errors: \n
-${Calc('zdorovki', 4, 2)}\n
-${Calc('sum', 'str', 2)}\n
-${Calc('sum', 4, true)}\n
+wrong action: ${Calc('zdorovki', 4, 2)}\n
+a - str: ${Calc('sum', 'str', 2)}\n
+b - Boolean: ${Calc('sum', 4, true)}\n
+b - NaN: ${Calc('sum', 4, NaN)}\n
+a - NaN: ${Calc('sum', NaN, 2)}\n
+a - Infinity: ${Calc('sum', Infinity, 2)}\n
+b - -Infinity: ${Calc('sum', 4, -Infinity)}\n
+divisible and remainder by 0: ${Calc('remainder', 4, 0)}\n
 \n
-actions: a = 4, b = 2\n
-sum: ${Calc('sum', 4, 2)}\n
-minus: ${Calc('minus', 4, 2)}\n
-multiply: ${Calc('multiply', 4, 2)}\n
-division: ${Calc('division', 4, 2)}\n
-remainder: ${Calc('remainder', 4, 2)}\n
-exponentiation: ${Calc('exponentiation', 4, 2)}\n
+actions: a = 5, b = 2\n
+sum: ${Calc('sum', 5, 2)}\n
+minus: ${Calc('minus', 5, 2)}\n
+multiply: ${Calc('multiply', 5, 2)}\n
+division: ${Calc('division', 5, 2)}\n
+remainder: ${Calc('remainder', 5, 2)}\n
+exponentiation: ${Calc('exponentiation', 5, 2)}\n
 `);
